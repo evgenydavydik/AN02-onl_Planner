b524fa6da11b3d73da18bf501d7722b1be2b4f6b
evgenydavydik <evgeny.davydik@gmail.com>
2021-04-07 21:17:00 +0300

    ДЗ выполнено редактирование существующей заметки.

---
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/add_note/AddNoteViewModel.kt
@@ -0,0 +1,6 @@
+package io.techmeskills.an02onl_plannerapp.screen.add_note
+
+import io.techmeskills.an02onl_plannerapp.support.CoroutineViewModel
+
+class AddNoteViewModel : CoroutineViewModel() {
+}
--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
@@ -2,10 +2,18 @@ package io.techmeskills.an02onl_plannerapp.screen.main
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.techmeskills.an02onl_plannerapp.R
 import io.techmeskills.an02onl_plannerapp.databinding.FragmentMainBinding
+import io.techmeskills.an02onl_plannerapp.screen.add_note.AddNoteFragment
 import io.techmeskills.an02onl_plannerapp.support.NavigationFragment
+import io.techmeskills.an02onl_plannerapp.support.RecyclerItemClickListener
+import io.techmeskills.an02onl_plannerapp.support.setVerticalMargin
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_main) {
@@ -14,22 +22,58 @@ class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_m
 
     private val viewModel: MainViewModel by viewModel()
 
+    private var positions: Int? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewBinding.buttonClick.setOnClickListener {
-            viewModel.text = viewBinding.editText.text.toString()
-            viewModel.doClickButton()
-            viewBinding.editText.setText("")
-        }
 
         viewModel.listLifeData.observe(this.viewLifecycleOwner, {
             viewBinding.recyclerView.adapter = NotesRecyclerViewAdapter(it)
         })
+
+        setFragmentResultListener(AddNoteFragment.ADD_NEW_RESULT) { key, bundle ->
+            val note = bundle.getString(AddNoteFragment.TEXT)
+            val date = bundle.getString(AddNoteFragment.DATE)
+            if (positions == null) {
+                note?.let {
+                    viewModel.addNote(it, date)
+                }
+            } else {
+                note?.let {
+                    viewModel.editNote(positions!!, it, date)
+                }
+            }
+
+        }
+
+
+
+
+        viewBinding.btnAdd.setOnClickListener {
+            it.findNavController().navigate(R.id.addNoteFragment)
+        }
+
+        viewBinding.recyclerView.addOnItemTouchListener(RecyclerItemClickListener(this.viewBinding.recyclerView,
+                object : RecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: View, position: Int) {
+                        view.findNavController().navigate(R.id.addNoteFragment)
+                        positions = position
+                        //Toast.makeText(activity, "$position item clicked!", Toast.LENGTH_LONG).show()
+                    }
+                }))
+
     }
 
     override fun onInsetsReceived(top: Int, bottom: Int, hasKeyboard: Boolean) {
         viewBinding.toolbar.setPadding(0, top, 0, 0)
         viewBinding.recyclerView.setPadding(0, 0, 0, bottom)
+        viewBinding.btnAdd.setVerticalMargin(marginBottom = bottom)
     }
+
+    override val backPressedCallback: OnBackPressedCallback
+        get() = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
 }
